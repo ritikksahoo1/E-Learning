be0e7c514e3bb5a9c37b3f4c92b8afa517b95a88
Varsini M <165877162+VarsiniMarimuthu@users.noreply.github.com>
2024-11-25 21:57:54 +0530

    Add files via upload

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  onSubmit() {
+    alert('Your message has been sent successfully!');
+  }
+}

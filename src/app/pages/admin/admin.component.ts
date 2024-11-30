0d0fb2cc02283ab3f14b61bf10b3c19b534e1b9c
VarsiniMarimuthu <varsinimarimuthu03@gmail.com>
2024-11-30 12:55:16 +0530

    manage courses

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-admin',
   standalone: true,
+  imports:[RouterModule],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  constructor(private router: Router) {}
+  navigateToCourse() {
+    this.router.navigate(['/course']); 
+  }
   onNavigate(section: string) {
     alert(`Navigating to ${section}!`);
+    
   }
 }

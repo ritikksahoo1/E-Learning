f47d042ff1276001202ee6c27d194bc9c95a5239
VarsiniMarimuthu <varsinimarimuthu03@gmail.com>
2024-11-30 13:27:35 +0530

    Added form validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 import { ApiService, LoginDetails } from '../../shared/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   loginForm: FormGroup;
@@ -16,14 +16,12 @@ export class LoginComponent {
   constructor(private fb: FormBuilder, private apiService: ApiService) {
     this.loginForm = this.fb.group({
       role: ['student', Validators.required], // Default value set to 'student'
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]], // Email validation
+      password: ['', [Validators.required, Validators.minLength(6)]], // Password validation
     });
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onSubmit(): void {
     if (this.loginForm.valid) {
@@ -33,10 +31,15 @@ export class LoginComponent {
         password: this.loginForm.value.password,
         role: this.loginForm.value.role
       };
-      this.apiService.login(payload).subscribe((response: any) => {
-        console.log(response);
-        this.apiService.token.set(response.token)
-      }, err => { console.log("Error occurred while fetching", err) });
+      this.apiService.login(payload).subscribe(
+        (response: any) => {
+          console.log(response);
+          this.apiService.token.set(response.token);
+        },
+        (err) => {
+          console.log('Error occurred while fetching', err);
+        }
+      );
     } else {
       console.log('Form is invalid');
     }

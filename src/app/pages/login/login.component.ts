a3d08f59cc0985b106039667540d8368630baf60
MUTHUKUMARSUBRAMANIYAN <115934345+MUTHUKUMARSUBRAMANIYAN@users.noreply.github.com>
2024-11-30 17:10:57 +0530

    changes done forgot password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,12 +12,15 @@ import { CommonModule } from '@angular/common';
 })
 export class LoginComponent {
   loginForm: FormGroup;
-
+  forgotPasswordForm: FormGroup;
   constructor(private fb: FormBuilder, private apiService: ApiService) {
     this.loginForm = this.fb.group({
       role: ['student', Validators.required], // Default value set to 'student'
       email: ['', [Validators.required, Validators.email]], // Email validation
       password: ['', [Validators.required, Validators.minLength(6)]], // Password validation
+    });
+    this.forgotPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
     });
   }
 
@@ -43,5 +46,20 @@ export class LoginComponent {
     } else {
       console.log('Form is invalid');
     }
+  }
+
+  onSendPassword(){
+    const payload = {
+      email: this.forgotPasswordForm.value.email
+    }
+    this.apiService.forgotPassword(payload).subscribe(
+      (response: any) => {
+        console.log(response);
+        this.apiService.token.set(response.token);
+      },
+      (err) => {
+        console.log('Error occurred while fetching', err);
+      }
+    );
   }
 }

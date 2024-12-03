0508fbd0fd85822f5e7eb5510e8cdc272d22bf00
Varsini M <165877162+VarsiniMarimuthu@users.noreply.github.com>
2024-12-03 10:53:42 +0530

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ApiService, LoginDetails } from '../../shared/api.service';
 import { CommonModule } from '@angular/common';
-
+ 
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -15,7 +15,7 @@ export class LoginComponent {
   forgotPasswordForm: FormGroup;
   constructor(private fb: FormBuilder, private apiService: ApiService) {
     this.loginForm = this.fb.group({
-      role: ['student', Validators.required], // Default value set to 'student'
+      // role: ['student', Validators.required], // Default value set to 'student'
       email: ['', [Validators.required, Validators.email]], // Email validation
       password: ['', [Validators.required, Validators.minLength(6)]], // Password validation
     });
@@ -23,16 +23,16 @@ export class LoginComponent {
       email: ['', [Validators.required, Validators.email]],
     });
   }
-
+ 
   ngOnInit(): void {}
-
+ 
   onSubmit(): void {
     if (this.loginForm.valid) {
       console.log('Form Submitted', this.loginForm.value);
       const payload: LoginDetails = {
-        userId: this.loginForm.value.email,
+        email: this.loginForm.value.email,
         password: this.loginForm.value.password,
-        role: this.loginForm.value.role
+        //role: this.loginForm.value.role
       };
       this.apiService.login(payload).subscribe(
         (response: any) => {
@@ -47,7 +47,7 @@ export class LoginComponent {
       console.log('Form is invalid');
     }
   }
-
+ 
   onSendPassword(){
     const payload = {
       email: this.forgotPasswordForm.value.email
